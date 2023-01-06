1b12a1427eefc63f9f8d8c762fc792df0930b3ab
Diego Jauregui Salvatierra <diego.jauregui@jala.university>
2023-01-06 12:51:15 +0000

    refactor: add git flow installation

--- postCreateCommand.sh
+++ postCreateCommand.sh
@@ -4,5 +4,6 @@
 sudo apt install make -y && sudo make
 # Change AWS Credentials Permissions
 sudo chmod 600 /home/vscode/.aws/credentials
-# Keybase
-# curl --remote-name https://prerelease.keybase.io/keybase_amd64.deb && sudo apt-get install -y ./keybase_amd64.deb
+# GitFlow
+sudo apt-get update
+sudo apt-get install git-flow
