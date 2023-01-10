6efb71a77aab440e5fcea096f9e786f26c24f781
Diego Jauregui Salvatierra <diego.jauregui@jala.university>
2023-01-10 14:48:37 +0000

    feat: jenkins instance creation and variables configurations

---
+++ terraform/modules/ec2_jenkins/variables.tf
@@ -0,0 +1,31 @@
+# Tagging system
+variable "tags" {
+  description = "Map of mass assign tags to all resources"
+  type        = map(string)
+}
+variable "resource_specific_tags" {
+  description = "Map of tags to assign to specific resources supporting tags. Merged with `tags`."
+  type        = map(map(string))
+
+  default = {}
+}
+
+# VPC
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+variable "subnet_id" {
+  description = "Id of subnet to connect to"
+  type        = string
+}
+/* variable "vpc_security_group_ids" {
+  description = "Id of VPC security group"
+  type        = string
+} */
+
+# Key Pair
+variable "aws_key_pair_name" {
+  description = "Key pair name"
+  type        = string
+}
--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,3 +1,7 @@
+output "aws_vpc_id" {
+  value = aws_vpc.this.id
+}
+
 output "aws_subnet_public_id" {
   value = aws_subnet.public.id
 }
