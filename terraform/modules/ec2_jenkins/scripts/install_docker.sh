b9692d45dba8ef65058dea66cbec3d980fab8d9b
Diego Jauregui Salvatierra <diego.jauregui@jala.university>
2023-01-12 20:01:47 +0000

    feat: docker and jenkins installation

--- terraform/modules/ec2_jenkins/scripts/install_docker.sh
+++ terraform/modules/ec2_jenkins/scripts/install_docker.sh
@@ -3,3 +3,6 @@
 sudo yum update -y
 sudo amazon-linux-extras install docker -y
 sudo usermod -a -G docker ec2-user
+sudo usermod -a -G docker jenkins
+sudo systemctl start docker
+sudo systemctl enable docker
